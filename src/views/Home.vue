04e4ffb2680c12db205efb46470f1f68ff4a9cc2
MinallW <suarezmiguelc23@gmail.com>
2021-06-23 19:06:01 -0500

    api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form>
+      <label>
+        Search:
+        <input v-model="tag" type="text" />
+      </label>
+      <button type="submit" class="go-button" @click.prevent="search">
+        Search
+      </button>
+    </form>
+    <p v-if="loading">Loading...</p>
+    <ul v-else>
+<li v-for="image in images" :key="image.id">
+  <img :src="image.url_n" :alt="image.title">
+  <div>
+    <p v-if="image.title">{{image.title}}</p>
+    <p v-else>No Title Found</p>
+    <p>By {{image.ownername}}</p>
+    <section>
+      <p>{{image.datetaken}}</p>
+      <p>Views: {{image.views}}</p>
+    </section>
+  </div>
+</li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios';
+import config from '../../config';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: 'home',
+  data() {
+    return {
+      loading: false,
+      tag: '',
+      images: [],
+    };
+  },
+  methods: {
+    search() {
+      this.loading = true;
+      this.fetchImages().then((response) => {
+        this.images = response.data.photos.photo;
+        this.loading = false;
+      });
+    },
+    fetchImages() {
+      return axios({
+        method: 'get',
+        url: 'https://api.flickr.com/services/rest',
+        params: {
+          method: 'flickr.photos.search',
+          api_key: config.api_key,
+          tags: this.tag,
+          extras: 'url_n, owner_name, date_taken, views',
+          page: 1,
+          format: 'json',
+          nojsoncallback: 1,
+          per_page: 30,
+        },
+      });
+    },
   },
 };
 </script>
