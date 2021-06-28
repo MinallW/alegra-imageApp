f38d8861aaf5918776c14b7248ff0cd2a6b69589
MinallW <suarezmiguelc23@gmail.com>
2021-06-28 05:36:10 -0500

    alegra quantity and improving winner component

--- src/components/Winner.vue
+++ src/components/Winner.vue
@@ -3,18 +3,25 @@
     <h2>Y, el ganador es:</h2>
     <h2>{{ seller }} </h2>
     <h3 v-if="winner">Alguien gano!</h3>
-    <p>¿Quieres hacer una factura?</p>
-    <button type="submit" class="btn btn--green btn--go" @click="createBill">
+    <p v-if="!generated">¿Quieres hacer una factura?</p>
+    <button type="submit" class="btn btn--green btn--go" @click="createBill" v-if="!generated">
       Hacer una factura
     </button>
+    <p v-if="generated">¡Generado correctamente!</p>
+    <button type="submit" class="btn btn--green btn--go" @click="goHome" v-if="generated">
+      Volver
+    </button>
+
   </div>
 </template>
 
 <script>
 import createInvoice, { checkSellers } from '../alegra';
+import store from '../store';
 
 export default {
   name: 'winner',
+  store,
   props: {
     nameIndex: Number,
   },
@@ -22,6 +29,7 @@ export default {
     return {
       seller: '',
       sellerId: null,
+      generated: false,
     };
   },
   created() {
@@ -38,17 +46,26 @@ export default {
           {
             id: 1,
             price: 12,
-            quantity: 5,
+            quantity: this.addQuantity(),
           },
         ],
       });
-      console.log(response);
+      if (response) {
+        this.generated = true;
+      }
+    },
+    addQuantity() {
+      const result = store.state.sellersPoints.reduce((a, b) => a + b, 0);
+      return result;
     },
     async fetchSellers() {
       const response = await checkSellers();
       const sellersNames = response.data;
       this.seller = sellersNames[this.nameIndex].name;
       this.sellerId = sellersNames[this.nameIndex].id;
+    },
+    goHome() {
+      this.$router.push({ name: 'Home' });
     },
   },
 };
